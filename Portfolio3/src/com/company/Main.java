da5c25a6131d53b9b0bd10b7fac2e542f333ebc2
k2carter2341 <kscarter2341@gmail.com>
2022-04-25 09:01:49 -0500

    Starting with Mads code on creating tables

--- Portfolio3/src/com/company/Main.java
+++ Portfolio3/src/com/company/Main.java
@@ -3,6 +3,54 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+    }
+
+    class Model {
+        MyDB db = new MyDB();   //add in external libraries
+
+        Model() {
+       /*
+
+        db.cmd("drop table if exists lst1;");
+        db.cmd("create table if not exists lst1 "+
+                "(fld1 integer primary key autoincrement, fld2 text);");
+
+        db.cmd("drop table if exists Courses;");
+        db.cmd("create table if not exists Courses "+
+                "(name text, stud integer);");
+        addCourses("Software Development","50");
+        addCourses("Essential Computing","90");
+
+        db.cmd("drop table if exists Rooms;");
+        db.cmd("create table if not exists Rooms "+
+                "(name text, stud integer);");
+        addRoom("10.2.49","60");
+        addRoom("10.1.25","30");
+
+        db.cmd("drop table if exists Timeslot;");
+        db.cmd("create table if not exists Timeslot "+
+                "(name text);");
+//        for(int i=1;i<=10;i++)addTimeslot("Slot "+i);
+        String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
+//        for(int i=1;i<=10;i++)addTimeslot("Slot "+i);
+        for(String day:days){addTimeslot(day+" AM");addTimeslot(day+" PM");}
+        db.cmd("drop table if exists Teacher;");
+        db.cmd("create table if not exists Teacher "+
+                "(name text);");
+        addLecturer("Mads Rosendahl");
+        addLecturer("Mickey Mouse");
+        addLecturer("Spiderman");
+
+        db.cmd("drop table if exists Room Booking;");
+        db.cmd("create table if not exists Room Booking "+
+                "(name text, Course ID, Time ID, Room ID);");
+
+        db.cmd("drop table if exists Teacher Booking;");
+        db.cmd("create table if not exists Teacher Booking "+
+                "(name text, Course ID, Time ID, Teacher ID);");
+         */
+
+        }
     }
 }
