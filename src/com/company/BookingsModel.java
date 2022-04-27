145011404c583b48af96f3dc0edf5a467c99bb2f
gho8888 <smgho@ruc.dk>
2022-04-27 14:59:36 +0200

    Deleted extra iml file and extracted src file. New schoolbookings.db db connection is working, some refactored variables

--- src/com/company/BookingsModel.java
+++ src/com/company/BookingsModel.java
@@ -5,69 +5,72 @@ import java.util.ArrayList;
 public class BookingsModel {
     BookingsDB db = new BookingsDB();   //add in external libraries
 
+
     BookingsModel() {
-        db.cmd("drop table if exists lst1;");
-        db.cmd("create table if not exists lst1 "+
+
+        db.cmdSQL("drop table if exists lst1;");
+        db.cmdSQL("create table if not exists lst1 "+
                 "(fld1 integer primary key autoincrement, fld2 text);");
 
-        db.cmd("drop table if exists Courses;");
-        db.cmd("create table if not exists Courses "+
+        db.cmdSQL("drop table if exists Courses;");
+        db.cmdSQL("create table if not exists Courses "+
                 "(name text, stud integer);");
         addCourses("Software Development","50");
         addCourses("Essential Computing","90");
 
-        db.cmd("drop table if exists Rooms;");
-        db.cmd("create table if not exists Rooms "+
+        db.cmdSQL("drop table if exists Rooms;");
+        db.cmdSQL("create table if not exists Rooms "+
                 "(name text, stud integer);");
         addRoom("10.2.49","60");
         addRoom("10.1.25","30");
 
-        db.cmd("drop table if exists Timeslot;");
-        db.cmd("create table if not exists Timeslot "+
+        db.cmdSQL("drop table if exists Timeslot;");
+        db.cmdSQL("create table if not exists Timeslot "+
                 "(name text);");
 //        for(int i=1;i<=10;i++)addTimeslot("Slot "+i);
         String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
 //        for(int i=1;i<=10;i++)addTimeslot("Slot "+i);
         for(String day:days){addTimeslot(day+" AM");addTimeslot(day+" PM");}
-        db.cmd("drop table if exists Teacher;");
-        db.cmd("create table if not exists Teacher "+
+        db.cmdSQL("drop table if exists Lecturer;");
+        db.cmdSQL("create table if not exists Lecturer "+
                 "(name text);");
         addLecturer("Mads Rosendahl");
         addLecturer("Mickey Mouse");
         addLecturer("Spiderman");
 
-        db.cmd("drop table if exists Room Booking;");
-        db.cmd("create table if not exists Room Booking "+
+        db.cmdSQL("drop table if exists RoomBooking;");
+        db.cmdSQL("create table if not exists RoomBooking "+
                 "(name text, Course ID, Time ID, Room ID);");
 
-        db.cmd("drop table if exists Teacher Booking;");
-        db.cmd("create table if not exists Teacher Booking "+
+        db.cmdSQL("drop table if exists TeacherBooking;");
+        db.cmdSQL("create table if not exists TeacherBooking "+
                 "(name text, Course ID, Time ID, Teacher ID);");
 
+        //PRAGMA foreign_keys = ON;
 
     }
 
-    void addLecturer(String s){  db.cmd("insert into Lecturer (name) values ('"+s+"');");}
-    ArrayList<String> getLecturer(){return db.query("select name from Lecturer;","name");}
+    void addLecturer(String s){  db.cmdSQL("insert into Lecturer (name) values ('"+s+"');");}
+    ArrayList<String> getLecturer(){return db.querySQL("select name from Lecturer;","name");}
 
     boolean hasLecturer(String s){
-        ArrayList<String> lst= db.query("select name from Lecturer where name = '"+s+"';","name");
+        ArrayList<String> lst = db.querySQL("select name from Lecturer where name = '"+s+"';","name");
         System.out.println(lst);
         return lst.size()>0;
         //return getLecturer().contains(s);
     }
 
-    void addRoom(String s,String stud){db.cmd("insert into Rooms (name,stud) values ('"+s+"',"+stud+");");}
-    ArrayList<String> getRoom(){return db.query("select name from Rooms;","name");}
+    void addRoom(String s,String stud){db.cmdSQL("insert into Rooms (name,stud) values ('"+s+"',"+stud+");");}
+    ArrayList<String> getRoom(){return db.querySQL("select name from Rooms;","name");}
 
-    void addCourses(String s,String stud){ db.cmd("insert into Courses (name,stud) values ('"+s+"',"+stud+");");}
+    void addCourses(String s,String stud){ db.cmdSQL("insert into Courses (name,stud) values ('"+s+"',"+stud+");");}
     ArrayList<String> getCourses(){
-        return db.query("select name from Courses;","name");
+        return db.querySQL("select name from Courses;","name");
     }
 
     String findRoom(String c){
-        ArrayList<String> lst= db.query(
-                "select Rooms.name from Rooms inner join Courses"
+        ArrayList<String> lst= db.querySQL(
+                "SELECT Rooms.name from Rooms inner join Courses"
                         +" where Courses.name = '"+c+"' and Rooms.stud > Courses.stud;","name");
         System.out.println(lst);
         if(lst.size()==0)return "";
@@ -75,17 +78,17 @@ public class BookingsModel {
     }
 
     void addTimeslot(String s){ // remember to sanitize your data!
-        db.cmd("insert into Timeslot (name) values ('"+s+"');");
+        db.cmdSQL("insert into Timeslot (name) values ('"+s+"');");
     }
     ArrayList<String> getTimeslot(){
-        return db.query("select name from Timeslot;","name");
+        return db.querySQL("select name from Timeslot;","name");
     }
 
     void add(String s){ // remember to sanitize your data!
-        db.cmd("insert into lst1 (fld2) values ('"+s+"');");
+        db.cmdSQL("insert into lst1 (fld2) values ('"+s+"');");
     }
     ArrayList<String> get(){
-        return db.query("select fld2 from lst1 order by fld1;","fld2");
+        return db.querySQL("select fld2 from lst1 order by fld1;","fld2");
     }
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,10 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private BookingsModel model=new BookingsModel();
-    private BookingsController con=new BookingsController(model,this);
-    private TextField field=new TextField();
-    private TextArea area=new TextArea();
+    private BookingsModel model = new BookingsModel();
+    private BookingsController controller = new BookingsController(model,this);
+    private TextField field = new TextField();
+    private TextArea area = new TextArea();
     ComboBox<String> lecturer = new ComboBox<>();
     ComboBox<String> courses = new ComboBox<>();
     ComboBox<String> rooms = new ComboBox<>();
@@ -20,15 +20,15 @@ public class Main extends Application {
     void clearField(){field.setText("");}
     @Override
     public void start(Stage stage) {
-        con.initArea();
-        field.setOnAction(e->con.enterText(field.getText()));
+        controller.initArea();
+        field.setOnAction(e->controller.enterText(field.getText()));
         VBox root = new VBox(courses,lecturer,rooms,timeslot,field,button,button2,area);
         lecturer.getItems().addAll(model.getLecturer());
         courses.getItems().addAll(model.getCourses());
         rooms.getItems().addAll(model.getRoom());
         timeslot.getItems().addAll(model.getTimeslot());
-        button.setOnAction(e->con.addLecturer(field.getText()));
-        button2.setOnAction(e->con.findRoom(courses.getValue()));
+        button.setOnAction(e->controller.addLecturer(field.getText()));
+        button2.setOnAction(e->controller.findRoom(courses.getValue()));
         Scene scene = new Scene(root, 500, 500);
         stage.setTitle("JavaFX Demo");
         stage.setScene(scene);
